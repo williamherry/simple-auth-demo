15f17cfba37e4e258a784a3fa5f91c04627e6fc4
William Herry <WilliamHerryChina@Gmail.com>
2013-06-28 01:04:46 +0800

    authentication from scratch

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Auth::Application.config.secret_key_base = '092febbe5e09bff5f17c2e1a378119d01df4d5e2f00ad5fe1cdb422df0a60800eaee3e02f80b6065f6f05de14808ca54c2e1f9b325f6003adcf9d3bb153e80d2'
